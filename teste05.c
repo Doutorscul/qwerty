ead0e10bfd2b94ed22a871b488938b8c5980087f
Doutorscul <127853626+Doutorscul@users.noreply.github.com>
2023-03-24 13:10:43 -0300

    Update teste05.c

---
+++ teste05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+   int num;
+
+   printf("Digite um número inteiro: ");
+   scanf("%d", &num);
+
+   if (num % 2 == 0) {
+      printf("O número %d é par.\n", num);
+   } else {
+      printf("O número %d é ímpar.\n", num);
+   }
+
+   return 0;
+}
